1f9b10461bce8787bba227a5e8e47d3edfd2da4c
yoonsele <yoonsele@student.42.fr>
2026-03-03 17:09:10 +0100

    create readme, change commen tin install_compiler.sh in english, add gitignore

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# How to run
+1. Install **i686-elf**
+```
+sh ./install_compiler.sh
+```
--- install_compiler.sh
+++ install_compiler.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-# i686-elf 크로스 컴파일러 자동 설치 (sudo 없이)
+# Install cross-compiler 'i686-elf' 
 
-echo "🚀 i686-elf 크로스 컴파일러 설치 시작..."
+echo "🚀 I will install cross-compiler, i686-elf..."
 
-# 디렉토리 생성
+# create directory
 mkdir -p /tmp/cross && cd /tmp/cross
 
-# zip 다운로드 & 압축 해제
+# download zip file and unzup it
 wget https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/i686-elf-tools-linux.zip
 unzip -q i686-elf-tools-linux.zip
 
 echo 'export PATH="/tmp/cross/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc  # 현재 셸 갱신
+source ~/.bashrc
 export PATH="/tmp/cross/bin:$PATH"
 
-# 확인
+# verify if it's installed correctly
 if i686-elf-gcc --version >/dev/null 2>&1; then
-    echo "✅ 설치 완료! GCC 버전:"
+    echo "✅ Installed successfully! with GCC version:"
     i686-elf-gcc --version | head -1
-    echo "이제 'make kfs-1.bin' 실행하세요!"
+    echo "now run 'make kfs-1.bin'"
 else
-    echo "❌ 설치 실패. bin/ 폴더 확인: ls ~/opt/cross/bin/"
+    echo "❌ Failed to install. check bin/ with: ls ~/opt/cross/bin/"
 fi
 
