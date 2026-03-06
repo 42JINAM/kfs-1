4ad4c8c938afa856e25b4e64627b8f70020416ee
christie26 <yoonseo9800@gmail.com>
2026-03-06 08:46:57 +0100

    change comment to english

--- src/boot.s
+++ src/boot.s
@@ -1,56 +1,55 @@
-; constants 
-	;;1. MBFLAGS 
-MBALIGN equ 1 << 0 
-MEMINFO equ 1 << 1
-MBFLAGS equ MBALIGN | MEMINFO
-		;; MBALIGN - 커널을 메모리에 복사할 때 페이지 시작점에 정렬되게 올림 
+; 1. MBFLAGS 
+; Declare constants for the multiboot header.
+;; MBALIGN - When copying the kernel into memory, align it to the start of a page
+;; MEMINFO - Scans the entire system memory map and passes it to the kernel
 
-		; 페이지 1: [    커널    | 12바이트 쓰레기]
-		; 페이지 2: [3KB 쓰레기  |     커널     ]
-		; ↓ MBALIGN 설정
-		; 페이지 1: [    커널(전체)     ]
-		; 페이지 2: [          빈 공간         ]
+MBALIGN  equ  1 << 0            ; align loaded modules on page boundaries
+MEMINFO  equ  1 << 1            ; provide memory map
+MBFLAGS  equ  MBALIGN | MEMINFO ; this is the Multiboot 'flag' field
+MAGIC    equ  0x1BADB002        ; 'magic number' lets bootloader find the header
+CHECKSUM equ -(MAGIC + MBFLAGS) ; checksum of above, to prove we are multiboot
+                                ; CHECKSUM + MAGIC + MBFLAGS should be Zero (0)
 
-		;; MEMINFO - 시스템 전체 메모리맵을 조사 해서 커널에 전달함 
+; Page 1: [    Kernel    | 12 bytes garbage]
+; Page 2: [ 3KB garbage  |     Kernel      ]
+; ↓ With MBALIGN enabled
+; Page 1: [      Kernel (entire)           ]
+; Page 2: [          Empty space           ]
 
-	;; 2. MAGIC NUMBER 
-MAGIC equ 0x1BADB002
-
-	;; 3. CHECKSUM
-CHECKSUM equ -(MAGIC + MBFLAGS)
 
 ; multiboot header 
+; GRUB searches the first 8 KiB in 32-bit mode for the magic number to recognize the kernel
 section .multiboot
 align 4
-		dd MAGIC
-		dd MBFLAGS
-		dd CHECKSUM
-		; dd : define double word - 4bytes 
-; GRUB이 32-bit -> 앞 8kib 에서 magic 을 찾아서 인식 
+  dd MAGIC
+  dd MBFLAGS
+  dd CHECKSUM
+  ; dd : define double word - 4 bytes
+
 
 ; Stack Setup for kernel entry
 section .bss
 align 16
 Stack_bottom:
-resb 16384 ; 16KiB
+resb 16384 ; 16 KiB
 stack_top:
 
 
 ; kernel entry point 
 section .text
-global _start:function (_start.end - _start) ;_start 함수 선언, 크기 지정
+global _start:function (_start.end - _start) ; declare _start function and specify its size
 _start:
-	;1. setup a stack
-	mov esp, stack_top
-	;1-2. etc GDT, floating point .... 
-	;아무튼 이제 C 함수를 부를 수 있음
-	;2. call kernel_main
-	extern kernel_main
-	call kernel_main
+  ;1. setup a stack
+  mov esp, stack_top
+  ;1-2. etc: GDT, floating point
+  ;now we can call C functions
 
-	;3. kernel termination loop
-	cli
-.hang: hlt
-	jmp .hang
+  ;2. call kernel_main
+  extern kernel_main
+  call kernel_main
+
+  ;3. kernel termination loop
+  cli
+.hang:  hlt
+  jmp .hang
 .end:
-
