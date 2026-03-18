f718e3cf70eed1fb024550fc5344947a62282919
42JINAM <jinam@student.42.fr>
2026-03-18 20:36:20 +0100

    gdt entry

--- src/gdt_setting.h
+++ src/gdt_setting.h
@@ -11,6 +11,16 @@
 
 #define NUM_GDT_ENTRIES 5
 
+//gdt entry
+typedef struct __attribute__((packed)) {
+    uint16_t limit_low;    // 0-15
+    uint16_t base_low;     // 16-31
+    uint8_t  base_mid;     // 32
+    uint8_t  access;       // 33-40 (type+S+DPL+P)
+    uint8_t  gran;         // 41-48 (limit_high+flags)
+    uint8_t  base_high;    // 49-56
+} gdt_entry_t;  //
+
 //gdtr
 typedef struct __attribute__((packed)) s_gdtr
 {
